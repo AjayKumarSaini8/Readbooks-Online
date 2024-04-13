03c504287ed578afdc458a79f065d94e39dc74fc
AjayKumarSaini8 <108985901+AjayKumarSaini8@users.noreply.github.com>
2024-04-13 11:19:05 +0530

    infinite scroll

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
 import { Link } from 'react-router-dom'
-
+import axios from 'axios'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const HomePage = () => {
     const [notes, setNotes] = useState([])
@@ -10,30 +11,34 @@ const HomePage = () => {
     const [error, setError] = useState(null)
     const { authTokens, logoutUser } = useContext(AuthContext)
 
-    useEffect(() => {
-        getNotes()
-    }, [])
-
+    const fetchMoreData = async () => {
+        if (query) {
+            try {
+                // Assuming you have a page variable to keep track of the pagination
+                const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=books+${query}&startIndex=${books.length}&maxResults=20&key=AIzaSyAbr_mnO88bXbeseUjO5aX1L2xXQCoVr_c`);
+                const data = response.data;
+                setBooks(prevBooks => [...prevBooks, ...(data.items || [])]);
+                setError(null);
+            } catch (error) {
+                setError(error.message);
+            }
+        }
+    };
     useEffect(() => {
         const fetchBooks = async () => {
             if (query) {
                 try {
-                    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=books+${query}&key=AIzaSyAbr_mnO88bXbeseUjO5aX1L2xXQCoVr_c`)
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! status: ${response.status}`)
-                    }
-                    const data = await response.json();
+                    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=books+${query}&key=AIzaSyAbr_mnO88bXbeseUjO5aX1L2xXQCoVr_c`);
+                    const data = response.data;
                     setBooks(data.items || []);
-                    setError(null)
+                    setError(null);
                 } catch (error) {
-                    setError(error.message)
+                    setError(error.message);
                 }
             }
         }
         fetchBooks();
     }, [query])
-
-
     const getNotes = async () => {
         const response = await fetch('http://127.0.0.1:8000/api/notes/', {
             method: 'GET',
@@ -49,42 +54,33 @@ const HomePage = () => {
             logoutUser()
         }
     }
+    useEffect(() => {
+        getNotes()
+    }, [])
     return (
-        <div>
-            <p>this is home page</p>
+        <div className="container mx-auto px-4">
+            <p className="text-2xl font-bold mb-4">Home Page</p>
 
-            <ul>
+            <ul className="space-y-2 mb-4">
                 {notes.map(note => (
-                    <li key={note.id}>{note.body}</li>
+                    <li key={note.id} className="border p-2 rounded">{note.body}</li>
                 ))}
             </ul>
-
             <div className="flex flex-col items-center justify-center">
-                <div class="flex">
+                <div className="flex w-full max-w-md mx-auto">
                     <input
                         type="text"
                         value={query}
                         onChange={(e) => setQuery(e.target.value)}
                         placeholder="Search for books..."
-                        class="
-      bg-gray-50 border border-gray-300 text-gray-900 text-sm
-      rounded-lg focus:ring-blue-500 focus:border-blue-500
-      block w-full ps-5 p-2.5 dark:bg-gray-700 dark:border-gray-600
-      dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-      dark:focus:border-blue-500
-    "
+                        className="flex-grow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                     <button
                         type="submit"
-                        class="
-      p-2.5 ms-2 text-sm font-medium text-white bg-blue-700
-      rounded-lg border border-blue-700 hover:bg-blue-800
-      focus:ring-4 focus:outline-none focus:ring-blue-300
-      dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
-    "
+                        className="p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                         <svg
-                            class="w-4 h-4"
+                            className="w-4 h-4"
                             aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
@@ -98,56 +94,56 @@ const HomePage = () => {
                                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                             />
                         </svg>
-                        <span class="sr-only">Search</span>
+                        <span className="sr-only">Search</span>
                     </button>
                 </div>
+                {error && <div className="text-red-500 mt-2">{error}</div>}
+                <InfiniteScroll
+                    dataLength={books.length}
+                    next={fetchMoreData}
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                >
+                    <div className="grid sm:grid-cols-1 xl:grid-cols-3 md:grid-cols-2  gap-4 mt-4">
+                        {books.map((book, index) => (
+                            book && book.volumeInfo && (
+                                <div key={index} className="bg-white p-4 shadow-md rounded transform transition duration-500 hover:bg-green-100">
+                                    {book.volumeInfo.imageLinks && (
+                                        <img
+                                            src={book.volumeInfo.imageLinks.smallThumbnail}
+                                            alt={book.volumeInfo.title}
+                                            className="mb-2 rounded h-48 object-cover"
+                                        />
+                                    )}
+                                    <h4 className="text-xl font-bold mb-2">{book.volumeInfo.title}</h4>
 
-
-
-
-                {error && <div className="text-red-500">{error}</div>}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    {books.map((book, index) => (
-                        <div key={index} className="bg-white p-4 shadow-md rounded">
-                            {book.volumeInfo.imageLinks && (
-                                <img
-                                    src={book.volumeInfo.imageLinks.smallThumbnail}
-                                    alt={book.volumeInfo.title}
-                                    className="mb-2 rounded"
-                                />
-                            )}
-
-                            <h4 className="text-xl font-bold">{book.volumeInfo.title}</h4>
-
-                            <p className="text-gray-600">
-                                {book.volumeInfo.authors && book.volumeInfo.authors.join(', ')}
-                            </p>
-
-                            <div className="mt-2">
-                                <a
-                                    href={book.volumeInfo.previewLink}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="bg-green-500 text-white px-2 py-1 rounded ml-2"
-                                >
-                                    See more
-                                </a>
-
-                                <Link to="/">
-                                    <button
-                                        type="button"
-                                        className="text-blue-500 hover:underline"
-                                    >
-                                        Add to Booklist
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                                    <p className="text-gray-600 mb-2">
+                                        {book.volumeInfo.authors && book.volumeInfo.authors.join(', ')}
+                                    </p>
+                                    <div className="mt-2 flex justify-between items-center">
+                                        <a
+                                            href={book.volumeInfo.previewLink}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="bg-green-500 text-white px-2 py-1 rounded hover:underline"
+                                        >
+                                            See more
+                                        </a>
+                                        <Link to="/">
+                                            <button
+                                                type="button"
+                                                className="text-blue-500 hover:underline"
+                                            >
+                                                Add to Booklist
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            )
+                        ))}
+                    </div>
+                </InfiniteScroll>
             </div>
-
-
         </div>
     )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Simple 4 column grid
+        '16': 'repeat(4, minmax(0, 1fr))',
+
+        // Complex site-specific column configuration
+        'footer': '200px minmax(900px, 1fr) 100px',
+      }
+    }
   },
   plugins: [],
 }
-
